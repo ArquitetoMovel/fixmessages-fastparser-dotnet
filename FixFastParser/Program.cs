21f2aa19a44fbccaddf85eb7f49f103abd386201
Alexandre Danelon <alexandre@MacBook-Air-de-Alexandre.local>
2024-05-25 20:18:38 -0300

    Voltando o Benchmark

--- FixFastParser/Program.cs
+++ FixFastParser/Program.cs
@@ -2,8 +2,7 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Loggers;
-using OpenTelemetry;
-using OpenTelemetry.Metrics;
+using BenchmarkDotNet.Running;
 
 namespace FixFastParser
 {
@@ -29,16 +28,12 @@ namespace FixFastParser
         [Benchmark]
         public void ParseMessage()
         {
-            _parseMessageCounter.Add(1, 
-                        new KeyValuePair<string, object?>("Id", Guid.NewGuid().ToString()));
             _fixParse.ParseMessage(_fixMessage);
         }
 
         [Benchmark]
         public void ParseMessageFaster()
         {
-            _parseMessageFasterCounter.Add(1, 
-                         new KeyValuePair<string, object?>("Id", Guid.NewGuid().ToString()));
             _fixParseFasterVersion.ParseMessage(_fixMessage);
             
         }
@@ -64,75 +59,20 @@ namespace FixFastParser
             config.AddLogger(_logger);
             config.AddColumnProvider();
             
-            using var meterProvider = Sdk.CreateMeterProviderBuilder()
-              .AddMeter("FixFastParser")
-              .SetMaxMetricPointsPerMetricStream(80_000)
-              
-              //.AddConsoleExporter()
-              .AddOtlpExporter((otlpExporter, metricsOptions) => 
-              {
-                otlpExporter.Endpoint = new Uri("http://localhost:4317"); // Replace with your OTLP collector endpoint  
-                metricsOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 2_000;
-                metricsOptions.TemporalityPreference = MetricReaderTemporalityPreference.Delta; ///
-              })
-              .Build();
+           //using var meterProvider = Sdk.CreateMeterProviderBuilder()
+           //  .AddMeter("FixFastParser")
+           //  .SetMaxMetricPointsPerMetricStream(80_000)
+           //  
+           //  //.AddConsoleExporter()
+           //  .AddOtlpExporter((otlpExporter, metricsOptions) => 
+           //  {
+           //    otlpExporter.Endpoint = new Uri("http://localhost:4317"); // Replace with your OTLP collector endpoint  
+           //    metricsOptions.PeriodicExportingMetricReaderOptions.ExportIntervalMilliseconds = 2_000;
+           //    metricsOptions.TemporalityPreference = MetricReaderTemporalityPreference.Delta; ///
+           //  })
+           //  .Build();
 
-            var client = new FixParseBenchmark();
-            client.UpdateQueue(0);
-            client.UpdateQueue(5);
-            Thread.Sleep(6_000);
-            client.UpdateQueue(30);
-            Thread.Sleep(6_000);
-            client.UpdateQueue(22);
-            Thread.Sleep(6_000);
-            client.UpdateQueue(10);
-            Thread.Sleep(6_000);
-            client.UpdateQueue(0);
-            client.UpdateQueue(2);
-
-    
-            // var decrement = 150;
-            // for (int i = 0; i < 300; i++)
-            // {
-            //     if (i <= 150)
-            //     {
-
-            //        client.UpdateQueue(i); 
-            //        System.Console.WriteLine($"Increment the queue {i}");
-            //     }else{
-            //       client.UpdateQueue(decrement--);
-            //       System.Console.WriteLine($"Decrement the queue {decrement}");
-            //     }
-            //     Thread.Sleep(2_000);
-
-            // }
-             //Parallel.For(0, 100_000, async i =>
-             //{
-               //for (int i = 0; i < 30_000; i++)
-               //{
-               //    client.ParseMessageFaster();
-               //    //client.ParseMessage();
-               //    if((i % 10_000) == 0) {
-               //        Console.WriteLine($" =====> delayed message {i} =====> {DateTime.Now}");
-               //        Task.Delay(30_000).Wait();
-               //    }
-
-               //   // if((i % 5000) == 0) {
-               //    //    Console.WriteLine($" =====> block send {i} =====> {DateTime.Now}");
-               //     //   Task.Delay(8_000).Wait();
-               //   // }
-
-
-               //}
-                //System.Console.WriteLine($"Clien iterating {i}");
-             //});       
-             //Aguard   
-          //while( Console.ReadKey().Key != ConsoleKey.G) {
-          // Task.Delay(16_000).Wait();
-          // System.Console.WriteLine("waiting for terminal");
-          // //client.ReleaseMetrics();
-          //// BenchmarkRunner.Run<FixParseBenchmark>(config);
-          //}
+           BenchmarkRunner.Run<FixParseBenchmark>(config);
         }
     }
 }
